7d465a26ab0a623e070f1ee9fddbe2b936e14bee
Szieberth Ádám <sziebadam@gmail.com>
2021-04-23 18:57:13 +0200

    new links

--- code/py/syntax_py.py
+++ code/py/syntax_py.py
@@ -8,6 +8,16 @@ font and are centered, all lines should have the same width to assure alignment.
 
 The script does the syntax highlighting and it also adds the necessary number of
 spaces to the end of each line.
+
+Usage:
+
+python syntax_py.py [margin] <mypyfile>
+
+Output goes to "<mypyfile>.html".
+
+Notes:
+
+* `pygments` Python module must be installed.
 """
 
 try:
@@ -21,28 +31,17 @@ else:
 import pathlib
 import sys
 
-usage = """
-Usage:
-
-python syntax_py.py [margin] <mypyfile>
-
-Output goes to "<mypyfile>.html".
-
-Notes:
-
-* `pygments` Python module must be installed."""
-
 if __name__ == "__main__":
 
     if pygments is None:
         print(f'ERROR! Third party module `pygments` is missing.')
         print(f'Hint: You can install it by calling `pip install pygments`.')
-        print(f'{__doc__}{usage}')
+        print(__doc__)
         sys.exit(1)
 
     if len(sys.argv) < 2:
         print(f'ERROR! Path of a python file is assumed as last argument.')
-        print(f'{__doc__}{usage}')
+        print(__doc__)
         sys.exit(2)
 
     margin = None
@@ -51,7 +50,7 @@ if __name__ == "__main__":
             margin = int(sys.argv[1])
         else:
             print(f'ERROR! Integer margin size is assumed as first argument.')
-            print(f'{__doc__}{usage}')
+            print(__doc__)
             sys.exit(3)
 
     p = pathlib.Path(sys.argv[-1]).resolve()
@@ -68,7 +67,7 @@ if __name__ == "__main__":
     if margin < maxcodelinelens :
         print("ERROR! Too low margin size!")
         print(f'Hint: Its minimum is the highest column number which is {maxcodelinelens}.')
-        print(f'{__doc__}{usage}')
+        print(__doc__)
         sys.exit(4)
     highlightedlinse = highlighted.split("\n")
     result = "\n".join(s + (" "*(margin-w) if w else "") for s, w in zip(highlightedlinse, codelinelens))
